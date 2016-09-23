bc347be754350e505e30ff4eb76c736db91b8aa3
Anton Shishkin <a.shishkin.b@gmail.com>
2016-09-23 13:55:45 +0300

    fix

--- lib/revisioner/agent_revision.rb
+++ lib/revisioner/agent_revision.rb
@@ -1,6 +1,6 @@
 module Revisioner
-    class AgentRevision < ActiveRecord::Base
-    self.table_name = Revisioner::Config.revision_table_name
+  class AgentRevision < ActiveRecord::Base
+    self.table_name = Revisioner::Config[:revision_table_name]
     has_many :agent_transactions, dependent: :destroy #, class_name: AgentTransaction
 
     attr_accessible :agent_code, :date_end, :date_start, :status, :data_file
--- lib/revisioner/agent_transaction.rb
+++ lib/revisioner/agent_transaction.rb
@@ -1,6 +1,6 @@
 module Revisioner
   class AgentTransaction < ActiveRecord::Base
-    self.table_name = Revisioner::Config.agent_transaction_table_name
+    self.table_name = Revisioner::Config[:agent_transaction_table_name]
 
     belongs_to :agent_revision #, class_name: Revisioner::AgentRevision
   end
