6ece843953f8591d3aefcd1c80b98da99ad36724
Anton Shishkin <a.shishkin.b@gmail.com>
2016-09-23 11:04:39 +0300

    requires

--- lib/revisioner/parser.rb
+++ lib/revisioner/parser.rb
@@ -1,12 +1,12 @@
 # @author Anton Shishkin
 # abstract class for report file parsing
 
-# require "revisioner/parsers/qiwi"
-require "parsers/qiwi"
-# require "revisioner/parsers/yandex"
-require "parsers/yandex"
-# require "revisioner/parsers/mobi"
-require "parsers/mobi"
+require "revisioner/parsers/qiwi"
+# require "parsers/qiwi"
+require "revisioner/parsers/yandex"
+# require "parsers/yandex"
+require "revisioner/parsers/mobi"
+# require "parsers/mobi"
 
 class Revisioner::Parser
   AGENT_QIWI = 1
