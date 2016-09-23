335c5c27f4babb393d5c2e8f00e9bdf06ed282f1
Anton Shishkin <a.shishkin.b@gmail.com>
2016-09-23 11:12:59 +0300

    qiwi

--- lib/revisioner/parser/qiwi.rb
+++ lib/revisioner/parser/qiwi.rb
@@ -1,16 +1,18 @@
 #encoding: utf-8
 # @author: Anton Shisnkin, Yuriy Soshin
-class Revisioner::Parser::Qiwi
-  HEADERS = [
-            "Статус",
-            "Дата выставления",
-            "Дата оплаты",
-            "ID счета",
-            "Visa QIWI Wallet",
-            "Сумма счета",
-            "Сумма оплаты",
-            "Сумма всех возвратов",
-            "Валюта",
-            "Комментарий"
-          ]
+module Revisioner::Parser
+  class Qiwi
+    HEADERS = [
+              "Статус",
+              "Дата выставления",
+              "Дата оплаты",
+              "ID счета",
+              "Visa QIWI Wallet",
+              "Сумма счета",
+              "Сумма оплаты",
+              "Сумма всех возвратов",
+              "Валюта",
+              "Комментарий"
+            ]
+  end
 end
